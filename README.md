df733aabe350525d85f4439fa156273b4469e4eb
mcra0009 <mcra0009@student.monash.edu>
2023-10-02 19:50:41 +1100

    Created commenter-GPT

---
+++ README.md
@@ -0,0 +1,38 @@
+# commenter-gpt README
+
+This Extension interfaces with chatgpt to allow you to easily add inline comments, docstrings, or both to your functions.
+
+## Features
+
+There are 3 commands currently implemented in this extension
+- Add Docstring
+- Add Inline Comments
+- Add Inline Comments and Docstring
+
+## Requirements
+In order to use this extension you have to have a valid ChatGPT API Key.
+
+Creating one can be done [here](https://platform.openai.com/account/api-keys)
+> This may require paying some amount for API tokens, however this extension uses ChatGPT version 3.5 therefore the usage is very small.
+
+Once you have gotten our **API Key**, you can add it to the extension with the following commands.
+1. Open Palette (Default Ctrl+Shift+P)
+2. Search "Add API Key"
+3. Enter API key
+
+
+## Known Issues
+
+This extension primarily uses ChatGPT AI Langauge Model, therefore there may be instances where as well as providing docstrings/inline comments, it also includes flavour text. However this is rare.
+
+## Release Notes
+
+Users appreciate release notes as you update your extension.
+
+### 1.0.0
+
+Initial release of Commenter-GPT
+
+---
+
+
---
+++ extension.js
@@ -0,0 +1,142 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+const { OpenAI } = require("openai");
+
+
+
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+	async function runCompletion (prompt) {
+		const api = new OpenAI({
+			apiKey: retrieveUserData("APIKey",context)
+		});
+		
+		const completion = await api.chat.completions.create({
+		model: "gpt-3.5-turbo",
+		messages: [{"role": "user", "content": prompt}],
+		max_tokens:4000
+	});
+	//console.log(completion);
+    return completion.choices[0].message.content;
+}
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "commenter-gpt" is now active!');
+
+	function saveUserData(key, value, context) {
+		const userData = { ...(context.globalState.get('userData') || {}), [key]: value };
+		context.globalState.update('userData', userData);
+	}
+	
+	function retrieveUserData(key, context) {
+		const userData = context.globalState.get('userData') || {};
+		return key ? userData[key] : userData;
+	}
+
+	context.subscriptions.push(vscode.commands.registerCommand('commenter-gpt.addAPIKey', () => {
+        vscode.window.showInputBox({
+            prompt: 'Enter your API key:',
+            placeHolder: `${retrieveUserData("APIKey",context)}`,
+        }).then(apiKey => {
+            if (apiKey) {
+				saveUserData("APIKey",apiKey,context)
+                vscode.window.showInformationMessage(`API Key ${retrieveUserData("APIKey",context)} has been stored.`);
+            }
+			else{
+                vscode.window.showInformationMessage('No API Key Provided');
+
+			}
+        });
+    }));
+
+	context.subscriptions.push(
+        vscode.commands.registerCommand('extension.addDocstring', async () => {
+            const editor = vscode.window.activeTextEditor;
+            if (editor) {
+				if (retrieveUserData("APIKey",context) != ""){
+					const selectedCode = editor.document.getText(editor.selection);
+                	const editBuilder = new vscode.WorkspaceEdit();
+					const newText = await runCompletion("Add docstrings to the following code provided, include a description, args, returns, don't include any explanations other than in the code in your response: " + selectedCode)
+                	editBuilder.replace(editor.document.uri, editor.selection,newText.replace(/^```python\s*/, '').replace(/\s*```$/, ''));
+
+                	// Apply the edit to the editor
+                	vscode.workspace.applyEdit(editBuilder);
+				}
+				else{
+					vscode.window.showErrorMessage("API Key not provided");
+				}
+            }
+        })
+    );
+
+	context.subscriptions.push(
+        vscode.commands.registerCommand('extension.addInlineComments', async () => {
+            const editor = vscode.window.activeTextEditor;
+            if (editor) {
+				if (retrieveUserData("APIKey",context) != ""){
+					const selectedCode = editor.document.getText(editor.selection);
+                	const editBuilder = new vscode.WorkspaceEdit();
+					const newText = await runCompletion("Add inline comments to the following code provided, don't include any explanations other than in the code in your response: " + selectedCode)
+                	editBuilder.replace(editor.document.uri, editor.selection,newText.replace(/^```python\s*/, '').replace(/\s*```$/, ''));
+
+                	// Apply the edit to the editor
+                	vscode.workspace.applyEdit(editBuilder);
+				}
+				else{
+					vscode.window.showErrorMessage("API Key not provided");
+				}
+            }
+        })
+    );
+
+	context.subscriptions.push(
+        vscode.commands.registerCommand('extension.addInlineandDocstring', async () => {
+            const editor = vscode.window.activeTextEditor;
+            if (editor) {
+				if (retrieveUserData("APIKey",context) != ""){
+					const selectedCode = editor.document.getText(editor.selection);
+                	const editBuilder = new vscode.WorkspaceEdit();
+					const newText = await runCompletion("Add docstrings and inline comments to the following code provided, include a description, args, returns in the docstrings. don't include any explanations other than in the code in your response: " + selectedCode)
+                	editBuilder.replace(editor.document.uri, editor.selection,newText.replace(/^```python\s*/, '').replace(/\s*```$/, ''));
+
+                	// Apply the edit to the editor
+                	vscode.workspace.applyEdit(editBuilder);
+				}
+				else{
+					vscode.window.showErrorMessage("API Key not provided");
+				}
+            }
+        })
+    );
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with  registerCommand
+	// The commandId parameter must match the command field in package.json
+	// let disposable = vscode.commands.registerCommand('commenter-gpt.addAPIKey', function () {
+	// 	// The code you place here will be executed every time your command is executed
+
+	// 	// Display a message box to the user
+	// 	const apiKeyInput = vscode.window.showInputBox({
+	// 		prompt: 'Enter your API key:',
+	// 		placeHolder: 'API Key',
+	// 	});
+
+		
+	// });
+}
+
+// This method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
